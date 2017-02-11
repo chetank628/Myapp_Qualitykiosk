fdb94226ea694eab812ce435737e64af4b5136a9
chetank628 <chetank628@gmail.com>
2017-02-11 20:18:00 +0530

    Adde google design element

--- app/src/main/java/com/qualitykiosk/myapp/MainActivity.java
+++ app/src/main/java/com/qualitykiosk/myapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.qualitykiosk.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
+        TextView mTitle = (TextView) toolbarTop.findViewById(R.id.toolbar_title);
+
     }
+
+
 }
+
+
