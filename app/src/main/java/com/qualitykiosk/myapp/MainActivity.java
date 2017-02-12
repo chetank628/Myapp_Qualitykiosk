c060ff65591a8693cc3afc752a132b5a3e7021c3
chetank628 <chetank628@gmail.com>
2017-02-12 15:16:44 +0530

    Changes to the loGin screen added spinner and backgrount tint to the button

--- app/src/main/java/com/qualitykiosk/myapp/MainActivity.java
+++ app/src/main/java/com/qualitykiosk/myapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.qualitykiosk.myapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         TextView mTitle = (TextView) toolbarTop.findViewById(R.id.toolbar_title);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        String[] items2 = new String[]{"","About us"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+                this,
+                R.layout.support_simple_spinner_dropdown_item,
+                items2
+        );
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner2.setAdapter(adapter2);
+
 
     }
 
 
 }
-
-
