9e94b0a23508dac20d25d823c974489d0b1122b6
chetank628 <chetank628@gmail.com>
2017-03-19 23:12:53 +0530

    Addded code to show password visibilty

--- app/src/main/java/com/qualitykiosk/myapp/MainActivity.java
+++ app/src/main/java/com/qualitykiosk/myapp/MainActivity.java
@@ -7,9 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton imagemenus;
     FloatingActionButton fabbutton;
+    private int passwordNotVisible=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,22 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         TextView mTitle = (TextView) toolbarTop.findViewById(R.id.toolbar_title);
+        ImageView showpasss = (ImageView) findViewById(R.id.iv_showpaasw);
 
-     imagemenus=(ImageButton)findViewById(R.id.imagemenu);
+        imagemenus = (ImageButton) findViewById(R.id.imagemenu);
         imagemenus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu popup =new PopupMenu(MainActivity.this,imagemenus);
-                popup.getMenuInflater().inflate(R.menu.popup_menu,popup.getMenu());
+                PopupMenu popup = new PopupMenu(MainActivity.this, imagemenus);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
                 popup.show();
-
 
 
             }
 
         });
 
+        showpasss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText paswword = (EditText) findViewById(R.id.et_password);
+                if (passwordNotVisible == 1) {
+                    paswword.setInputType( InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordNotVisible = 0;
+                } else {
 
+                    paswword.setInputType( InputType.TYPE_CLASS_TEXT |InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordNotVisible = 1;
+                }
+
+
+                paswword.setSelection(paswword.length());
+
+            }
+        });
     }
 
 
